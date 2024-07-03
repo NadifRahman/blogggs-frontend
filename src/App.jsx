321ac26227038c44093a50bd7f137627e49f5f45
NadifRahman <nadif.rah@gmail.com>
2024-07-02 22:32:45 -0400

    Create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './bootstrap.min.css';
+import Navbar from './components/general/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Outlet />
     </>
   );
---
+++ src/components/general/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavStyle from './navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark
+     bg-primary px-3 sticky-top"
+    >
+      <Link className="navbar-brand" to="/">
+        Bloggs
+      </Link>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item nav-link ">
+          <Link className={NavStyle.link} to="/log-in">
+            Log in
+          </Link>
+        </li>
+        <li className="nav-item nav-link">
+          <Link className={NavStyle.link} to="/sign-up">
+            Sign up
+          </Link>{' '}
+          {/*come back and add log out button which just deletes the token */}
+        </li>
+      </ul>
+    </nav>
+  );
+}
