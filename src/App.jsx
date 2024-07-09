86e21a6f96661086a46b1a6690350a4bb1b11537
NadifRahman <nadif.rah@gmail.com>
2024-07-08 22:03:12 -0400

    Make the navbar dynamic
    - Navbar displays different thing based on if user is logged in or not

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar jwtToken={jwtToken} logOut={logOut}></Navbar>
       <Outlet context={{ jwtToken, setToken, logOut }} />
     </>
   );
--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavStyle from './navbar.module.css';
 
-export default function Navbar() {
+export default function Navbar({ jwtToken, logOut }) {
   return (
     <nav
       className="navbar navbar-expand-lg navbar-dark
@@ -11,18 +11,31 @@ export default function Navbar() {
       <Link className="navbar-brand" to="/">
         Bloggs
       </Link>
+
       <ul className="navbar-nav mr-auto">
-        <li className="nav-item nav-link ">
-          <Link className={NavStyle.link} to="/log-in">
-            Log in
-          </Link>
-        </li>
-        <li className="nav-item nav-link">
-          <Link className={NavStyle.link} to="/sign-up">
-            Sign up
-          </Link>
-          {/*come back and add log out button which just deletes the token */}
-        </li>
+        {jwtToken ? (
+          <li
+            className="nav-item nav-link"
+            style={{ color: 'white', cursor: 'pointer' }}
+            onClick={logOut}
+          >
+            Log out
+          </li>
+        ) : (
+          <>
+            <li className="nav-item nav-link ">
+              <Link className={NavStyle.link} to="/log-in">
+                Log in
+              </Link>
+            </li>
+            <li className="nav-item nav-link">
+              <Link className={NavStyle.link} to="/sign-up">
+                Sign up
+              </Link>
+              {/*come back and add log out button which just deletes the token */}
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
